5f3892ac74396c742c9d592ab889240239b52d82
Viktor <lubarskyvik@gmail.com>
2018-02-06 11:43:38 +0200

    angular ng features

--- src/app/pages/table/table.module.ts
+++ src/app/pages/table/table.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import {FormsModule} from "@angular/forms";
+
 import { TableComponent } from './table/table.component'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [TableComponent]
 })
--- src/app/pages/table/table/table.component.ts
+++ src/app/pages/table/table/table.component.ts
@@ -5,10 +5,14 @@ import { Component, OnInit } from '@angular/core'
   templateUrl: './table.component.html'
 })
 export class TableComponent implements OnInit {
+  dirty = false
+  inputVal = "blabla"
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.dirty = true
+    }, 1000 * 5)
   }
-
 }
